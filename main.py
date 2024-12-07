e9aba85909af604a8d07b8e130fcc75e3931f130
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-12-07 14:03:50 -0500

    Add invoice number and date to the PDF files.

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ filepaths = glob.glob("files/xlsx/*.xlsx")
 
 for filepath in filepaths:
     filename = pathlib.Path(filepath).stem
+    invoice_number, invoice_date = filename.split("-")
 
     pdf = FPDF("portrait", "mm", "A4")
     pdf.set_font("Times", "B", 16)
+
     pdf.add_page()
+    pdf.cell(0, 8, f"Invoice: #{invoice_number}", 0, 1)
+    pdf.cell(0, 8, f"Date: {invoice_date}", 0, 1)
     pdf.output(f"files/pdf/{filename}.pdf")
